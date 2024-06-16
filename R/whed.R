8054dfb7b6ad21ad7da857be9828daea0c8bf754
pablogguz <72149928+pablogguz@users.noreply.github.com>
2024-06-16 22:30:18 +0200

    first commit

---
+++ R/whed.R
@@ -0,0 +1,26 @@
+#' WHED Dataset
+#'
+#' This dataset contains institution-level information on higher education institutions around the world.
+#'
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{name}{Name of the institution}
+#'   \item{url}{Website URL of the institution}
+#'   \item{country}{Country where the institution is located}
+#'   \item{id}{Unique identifier for the institution}
+#'   \item{city}{City where the institution is located}
+#'   \item{name_detail}{Detailed name of the institution}
+#'   \item{funding_type}{Type of funding for the institution (e.g., public, private)}
+#'   \item{languages}{Languages used at the institution}
+#'   \item{accrediting_agency}{Accrediting agency for the institution}
+#'   \item{year_founded}{Year the institution was founded}
+#'   \item{academic_year}{Academic year details}
+#'   \item{business_school}{Indicates if the institution has a business school}
+#'   \item{degree_count}{Number of degrees offered}
+#'   \item{division_name_X}{Name of division X}
+#'   \item{division_fields_study_X}{Fields of study in division X}
+#'   \item{degree_name_X}{Name of degree X}
+#'   \item{degree_fields_study_X}{Fields of study for degree X}
+#' }
+#' @source Source of the data
+"whed"
